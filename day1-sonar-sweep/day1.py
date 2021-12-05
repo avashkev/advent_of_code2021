5e26b8c52e687a3698d318335ab9184b0e8431a5
anastasia <anastasi@98point6.com>
2021-12-05 14:33:54 -0800

    added rolling window to sum depths

--- day1-sonar-sweep/day1.py
+++ day1-sonar-sweep/day1.py
@@ -3,40 +3,49 @@
 import sys
 
 def main(argv):
-    #get the input file
-    inputFile = open(argv[0],'r')
+   #get the input file
+ #  inputFile = open('C:\\Users\\Nastasia\\Documents\\src\\advent_of_code2021\\day1-sonar-sweep\\test.txt','r')
+   inputFile = open(argv[0],'r')
 
-    #read tthe input file
-    content = inputFile.readlines() 
-    inputFile.close()
-    
-    #array of depths
-    depths = []
+   #read tthe input file
+   content = inputFile.readlines() 
+   inputFile.close()
+   
+   #array of depths
+   depths = []
 
-    #counter for the number of increases  
-    increases = 0
-    decreases = 0
+   #counter for the number of increases  
+   increases = 0
+   decreases = 0
 
-    # Iterating through the depths
-    for line in content: 
-        depths.append(int(line.strip('\n')))
-    
+   # Iterating through the depths
+   for line in content: 
+      depths.append(int(line.strip('\n')))
+   
+   #the number of depths that will be added together and compared
+   window = 3
 
-    #iteratte through the array and count increases
-    for indx in range(1, len(depths)):
-        if depths[indx] > depths[indx-1]:
-           # print(str(depths[indx]) + '>' + str(depths[indx-1]))
-            increases += 1
-           # print('up: ' + str(increases))
-        else: 
-           # print(str(depths[indx]) + '<' + str(depths[indx-1]))
-            decreases +=1
-           # print('down: ' + str(decreases))
+   #get the first set of depths
+   prevDepth = 0
+   for i in range(0, window):
+         prevDepth += depths[i]
+
+   #iteratte through the array and count increases
+   for indx in range(window, len(depths)):
+      currDepth = 0
+      for i in range(0, window):
+         currDepth += depths[indx-i]
+         
+      if prevDepth < currDepth:
+         increases += 1
+
+      prevDepth = currDepth
 
 
-    print('total:')
-    print(increases)
-   # print(decreases)
+
+   print('total:')
+   print("Total Increases: " + str(increases))
+# print(decreases)
 
 
 if __name__ == "__main__":
